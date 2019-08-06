e394c790c9c42c3fd1a152c5d89409ea997c2478
dexter199057@gmail.com <dexter1990@gmail.com>
2019-08-06 14:25:07 +0800

    修复home

---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        <Homeswiper />
+        <Notice />
+        <QuickFuc />
+    </div>
+</template>
+
+<script>
+import Homeswiper from '@/components/home/homeSwiper'
+import Notice from '@/components/home/notice'
+import QuickFuc from '@/components/home/quickFuc'
+export default {
+    components: {
+        Homeswiper,
+        Notice,
+        QuickFuc
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.home
+    height 80000px
+</style>
