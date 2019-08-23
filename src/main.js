2779b817783258c2f43d906c96caa086e5cde8dd
dexter199057@gmail.com <dexter1990@gmail.com>
2019-08-23 11:54:46 +0800

    修复打包体积过大

--- src/main.js
+++ src/main.js
@@ -4,19 +4,23 @@ import router from './router'
 import store from './store'
 //import { AlertPlugin, ToastPlugin ,ConfirmPlugin,LoadingPlugin } from 'vux'
 import VuePageTransition from 'vue-page-transition'
-import  { ConfirmPlugin,Countup } from 'vux'
-import VueClipboard from 'vue-clipboard2'
-import ToastPlugin from 'vux/src/plugins/toast'
-Vue.use(ConfirmPlugin)
 Vue.use(VuePageTransition)
-Vue.component('countup', Countup)
 
-VueClipboard.config.autoSetContainer = true
+import  Countup  from 'vux/src/components/countup'
+import ConfirmPlugin  from 'vux/src/plugins/confirm'
+Vue.component('countup', Countup)
+Vue.use(ConfirmPlugin)
+
+import VueClipboard from 'vue-clipboard2'
 Vue.use(VueClipboard)
+
+import ToastPlugin from 'vux/src/plugins/toast'
 Vue.use(ToastPlugin, {
     position: 'middle',
     width: '50%'
 })
+VueClipboard.config.autoSetContainer = true
+
 
 Vue.config.productionTip = false
 
